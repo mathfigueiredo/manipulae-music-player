ede8b75dcb9f299eb8310a584b5ca03ef277851e
Matheus Figueiredo <mathhsoares2@live.com>
2021-08-18 00:20:56 -0300

    add GlobalStyle with styled components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React from 'react';
 import Home from '../pages/Home';
+import SongDetails from './SongDetails';
+import GlobalStyle from './GlobalStyle';
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <Home />
+        <GlobalStyle />
+        {/* <Home /> */}
+        <SongDetails />
       </div>
     );
   }
