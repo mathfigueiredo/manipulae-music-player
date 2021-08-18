0a749d8025d48a674a4786c46791f28c8f503587
Matheus Figueiredo <mathhsoares2@live.com>
2021-08-17 22:14:06 -0300

    Refactor SearchBar.js

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -2,24 +2,16 @@ import React from 'react';
 import { Field, reduxForm } from 'redux-form';
 import { connect } from 'react-redux';
 import { fetchSearchResults } from '../actions';
-import deezer from '../api/deezer';
 
 class SearchBar extends React.Component {
   renderInput(formProps) {
     return <input {...formProps.input} />;
   }
 
-  onSearchChange = (e) => {
-    if (e.target.value !== '') {
-      if (this.timer) clearTimeout(this.timer);
-      this.timer = setTimeout(() => this.props.fetchSearchResults(e), 500);
-    }
-  };
-
   render() {
     return (
       <form>
-        <Field name="query" component={this.renderInput} onChange={this.onSearchChange} />
+        <Field name="query" component={this.renderInput} onChange={this.props.onSearchChange} />
       </form>
     );
   }
