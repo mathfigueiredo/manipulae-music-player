7ce24da619dc9f5b0e866af4da359ce2bf875c4e
Matheus Figueiredo <mathhsoares2@live.com>
2021-08-17 23:33:18 -0300

    Update SongsList so it renders the SongCards properly

--- src/components/SongsList.js
+++ src/components/SongsList.js
@@ -1,9 +1,29 @@
 import React from 'react';
+import SongCard from './SongCard';
+import { connect } from 'react-redux';
 
 class SongsList extends React.Component {
+  renderList = () => {
+    const { songs } = this.props;
+    return songs.map((song) => {
+      return (
+        <SongCard
+          key={song.id}
+          id={song.id}
+          title={song.title}
+          duration={song.duration}
+          preview={song.preview}
+          artist={song.artist}
+          album={song.album}
+          image={song.md5_image}
+        />
+      );
+    });
+  };
+
   render() {
-    return <div>SongsList</div>;
+    return <div>{this.renderList()}</div>;
   }
 }
 
-export default SongsList;
+export default connect()(SongsList);
