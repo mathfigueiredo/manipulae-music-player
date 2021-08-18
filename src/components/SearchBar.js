ba849597f6e7e1de287972d2b0151fd20d096f03
Matheus Figueiredo <mathhsoares2@live.com>
2021-08-17 21:39:36 -0300

    Develop SearchBar.js component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import { Field, reduxForm } from 'redux-form';
+import { connect } from 'react-redux';
+import deezer from '../api/deezer';
 
 class SearchBar extends React.Component {
+  renderInput(formProps) {
+    return <input {...formProps.input} />;
+  }
+
+  onSearchChange = (e) => {
+    if (e.target.value !== '') {
+      if (this.timer) clearTimeout(this.timer);
+      this.timer = setTimeout(() => {
+        deezer
+          .get('/search', { params: { q: e.target.value } })
+          .then((res) => console.log(res.data));
+      }, 500);
+    }
+  };
+
   render() {
-    return <div>SearchBar</div>;
+    return (
+      <form>
+        <Field name="query" component={this.renderInput} onChange={this.onSearchChange} />
+      </form>
+    );
   }
 }
 
-export default SearchBar;
+const form = reduxForm({
+  form: 'searchForm',
+})(SearchBar);
+
+export default form;
