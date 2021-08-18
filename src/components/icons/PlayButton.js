9ad02a8f1fc45d8966226c02fad187872c17d90d
Matheus Figueiredo <mathhsoares2@live.com>
2021-08-17 23:44:23 -0300

    add Buttons SVG

---
+++ src/components/icons/PlayButton.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+class PlayButton extends React.Component {
+  render() {
+    return (
+      <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+        <path d="M3 22v-20l18 10-18 10z" />
+      </svg>
+    );
+  }
+}
+
+export default PlayButton;
